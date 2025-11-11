40fab83630ee354f749ed6c4207039a76c7c66cc
Dante Neckmar <da6404ne-s@student.lu.se>
2025-11-11 20:13:56 +0100

    Changes made to CAD

--- MotorTest/MotorTest.ino
+++ MotorTest/MotorTest.ino
@@ -1,63 +1,96 @@
 // --- Pin Setup ---
-const int motorPWMa = 5;      // PWM pin to motor driver
-const int motorPWMb = 6;      // Direction pin to motor driver
+const int motorPWM1a = 6;      // PWM pin to motor driver
+const int motorPWM1b = 9;      // Direction pin to motor driver
+const int motorPWM2a = 10;      // PWM pin to motor driver
+const int motorPWM2b = 11;      // Direction pin to motor driver
 
-const int encoderA = 2;      // Encoder channel A (must be interrupt-capable)
-const int encoderB = 3;      // Encoder channel B (interrupt-capable)
+const int encoder1A = 2;      // Encoder channel A (must be interrupt-capable)
+const int encoder1B = 3;      // Encoder channel B (interrupt-capable)
+const int encoder2A = 4;      // Encoder channel A (must be interrupt-capable)
+const int encoder2B = 5;      // Encoder channel B (interrupt-capable)
 
 int PWMspeed = 20; 
 
 // Encoder count
-volatile long encoderCount = 0;
+volatile long encoderCount1 = 0;
+volatile long encoderCount2 = 0;
 
 // Decode direction from encoder
-void encoderA_ISR() {
-  int stateA = digitalRead(encoderA);
-  int stateB = digitalRead(encoderB);
+void encoder1A_ISR() {
+  int stateA = digitalRead(encoder1A);
+  int stateB = digitalRead(encoder1B);
 
   // If A leads B, forward; else reverse
-  if (stateA == stateB) encoderCount++;
-  else encoderCount--;
+  if (stateA == stateB) encoderCount1++;
+  else encoderCount1--;
 }
 
-void encoderB_ISR() {
-  int stateA = digitalRead(encoderA);
-  int stateB = digitalRead(encoderB);
+void encoder1B_ISR() {
+  int stateA = digitalRead(encoder1A);
+  int stateB = digitalRead(encoder1B);
 
-  if (stateA != stateB) encoderCount++;
-  else encoderCount--;
+  if (stateA != stateB) encoderCount1++;
+  else encoderCount1--;
+}
+
+void encoder2A_ISR() {
+  int stateA = digitalRead(encoder2A);
+  int stateB = digitalRead(encoder2B);
+
+  // If A leads B, forward; else reverse
+  if (stateA == stateB) encoderCount2++;
+  else encoderCount2--;
+}
+
+void encoder2B_ISR() {
+  int stateA = digitalRead(encoder2A);
+  int stateB = digitalRead(encoder2B);
+
+  if (stateA != stateB) encoderCount2++;
+  else encoderCount2--;
 }
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(motorPWMa, OUTPUT);
-  pinMode(motorPWMb, OUTPUT);
+  pinMode(motorPWM1a, OUTPUT);
+  pinMode(motorPWM1b, OUTPUT);
+  pinMode(motorPWM2a, OUTPUT);
+  pinMode(motorPWM2b, OUTPUT);
 
-  pinMode(encoderA, INPUT_PULLUP);
-  pinMode(encoderB, INPUT_PULLUP);
+  pinMode(encoder1A, INPUT_PULLUP);
+  pinMode(encoder1B, INPUT_PULLUP);
+  pinMode(encoder2A, INPUT_PULLUP);
+  pinMode(encoder2B, INPUT_PULLUP);
+  
 
-  attachInterrupt(digitalPinToInterrupt(encoderA), encoderA_ISR, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(encoderB), encoderB_ISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encoder1A), encoder1A_ISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encoder1B), encoder1B_ISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encoder2A), encoder2A_ISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encoder2B), encoder2B_ISR, CHANGE);
 
   // Start motor forward
-  analogWrite(motorPWMb, 0);  // HIGH = forward, change if reversed
-  analogWrite(motorPWMa, 50);    // Speed (0–255)
+  analogWrite(motorPWM1b, 0);  // HIGH = forward, change if reversed
+  analogWrite(motorPWM1a, 50);    // Speed (0–255)
+  analogWrite(motorPWM2b, 0);  // HIGH = forward, change if reversed
+  analogWrite(motorPWM2a, 50); 
 }
 
 void loop() {
   static long lastCount = 0;
 
   // Print only if count changed (keeps Serial clean)
-  if (encoderCount != lastCount) {
-    Serial.print("Encoder Count: ");
-    Serial.println(encoderCount/3840);
-    lastCount = encoderCount;
+  if (encoderCount1 != lastCount) {
+    Serial.print("Encoder Count 1: ");
+    Serial.println(encoderCount1/3840);
+    Serial.print("Encoder Count 2: ");
+    Serial.println(encoderCount2/3840);
+    lastCount = encoderCount1;
   }
 
   // Optional: after some time, reverse direction
-  if (millis() > PWMspeed*100) {
-    analogWrite(motorPWMa, PWMspeed);    // Speed (0–255)
+  if (millis() > PWMspeed*100 && PWMspeed < 255) {
+    analogWrite(motorPWM1a, PWMspeed);    // Speed (0–255)
     PWMspeed = PWMspeed + 20; 
   }
 }
